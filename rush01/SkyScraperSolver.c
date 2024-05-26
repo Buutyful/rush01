29dc7405f4bea6d664be372b698a463965d9aa74
Buutyful <mantelli_git@libero.it>
2024-05-26 03:14:58 +0200

    need testing cases

--- rush01/SkyScraperSolver.c
+++ rush01/SkyScraperSolver.c
@@ -321,7 +321,7 @@ int Solve(int** board, int size, Hints* hints)
 {
 	int row = -1;
 	int col = -1;
-	int isEmpty = 1;
+	int isEmpty = 0;
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
@@ -330,15 +330,15 @@ int Solve(int** board, int size, Hints* hints)
 			{
 				row = i;
 				col = j;
-				isEmpty = 0;
+				isEmpty = 1;
 				break;
 			}
 		}
-		if (isEmpty == 0)
+		if (isEmpty)
 			break;
 	}
 
-	if (isEmpty)
+	if (!isEmpty)
 		return 1;
 
 	for (int num = 1; num <= size; num++)
